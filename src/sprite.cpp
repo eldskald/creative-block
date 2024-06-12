9bfe21460e882a03b4b2af36362aa2c9728d10b1
eldskald <rafaelbordoni00@gmail.com>
2024-06-12 01:01:29 -0300

    Refactor animations

--- src/sprite.cpp
+++ src/sprite.cpp
@@ -8,6 +8,15 @@ void sprite::initialize() {
 }
 
 void sprite::tick_() {
+    if (this->anim.size() > 0) {
+        this->curr_time_ += GetFrameTime();
+        while (this->curr_time_ >= this->anim.at(this->curr_frame_).duration) {
+            this->curr_time_ -= this->anim.at(this->curr_frame_).duration;
+            this->curr_frame_ =
+                (this->curr_frame_ + 1) % (int)this->anim.size();
+            this->atlas_coords = this->anim.at(this->curr_frame_).atlas_coords;
+        }
+    }
     BeginShaderMode(*(this->shader));
     DrawTexturePro(sprite::atlas_,
                    (Rectangle){SPRITESHEET_CELL_SIZE_X * this->atlas_coords.x,
--- src/sprite.h
+++ src/sprite.h
@@ -2,11 +2,20 @@
 #include "game-element.h"
 #include "shader.h"
 #include <raylib.h>
+#include <vector>
+
+using keyframe = struct {
+    Vector2 atlas_coords;
+    float duration;
+};
+
+using animation = vector<keyframe>;
 
 class sprite : public game_element {
 public:
     Vector2 atlas_coords{(Vector2){0}};
     Color tint{WHITE};
+    animation anim;
     Shader* shader{shader::get_base()};
 
     static void initialize();
@@ -15,5 +24,8 @@ protected:
     void tick_() override;
 
 private:
+    int curr_frame_{0};
+    float curr_time_{0.0f};
+
     static Texture2D atlas_;
 };
