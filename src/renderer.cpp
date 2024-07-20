558914dfd20cfb2ddaa2e4acd3c0e5c6847140e2
eldskald <rafaelbordoni00@gmail.com>
2024-07-19 22:47:03 -0300

    Remove maximum resolution

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -8,14 +8,15 @@ using namespace std;
 RenderTexture2D renderer::base_tex_step_1_ = (RenderTexture2D){0};
 RenderTexture2D renderer::base_tex_ = (RenderTexture2D){0};
 RenderTexture2D renderer::stretched_tex_ = (RenderTexture2D){0};
+Vector2 renderer::stretched_tex_size_ = (Vector2){0};
 
 void renderer::initialize() {
     // For the sake of the web build, all textures must be powers of 2
     renderer::base_tex_step_1_ =
         LoadRenderTexture(MAIN_TEX_SIZE, MAIN_TEX_SIZE);
     renderer::base_tex_ = LoadRenderTexture(MAIN_TEX_SIZE, MAIN_TEX_SIZE);
-    renderer::stretched_tex_ =
-        LoadRenderTexture(MAX_RESOLUTION, MAX_RESOLUTION);
+    renderer::stretched_tex_ = LoadRenderTexture(2, 2);
+    renderer::stretched_tex_size_ = (Vector2){2.0f, 2.0f};
 }
 
 void renderer::render() {
@@ -24,12 +25,41 @@ void renderer::render() {
     BeginDrawing();
     ClearBackground(BLACK);
     DrawTexturePro(renderer::stretched_tex_.texture,
-                   (Rectangle){0, 0, MAX_RESOLUTION, -MAX_RESOLUTION},
-                   (Rectangle){0, 0, MAX_RESOLUTION, MAX_RESOLUTION},
+                   (Rectangle){0,
+                               0,
+                               renderer::stretched_tex_size_.x,
+                               -renderer::stretched_tex_size_.y},
+                   (Rectangle){0,
+                               0,
+                               renderer::stretched_tex_size_.x,
+                               renderer::stretched_tex_size_.y},
                    (Vector2){0, 0},
                    0.0f,
                    WHITE);
     EndDrawing();
+}
+
+void renderer::update_stretched_tex_size_() {
+    float window_size_x = (float)GetRenderWidth();
+    float window_size_y = (float)GetRenderHeight();
+    bool increased_size = false;
+
+    // Multiplying sizes by 2 so they remain powers of 2
+    while (renderer::stretched_tex_size_.x < window_size_x) {
+        increased_size = true;
+        renderer::stretched_tex_size_.x *= 2;
+    }
+    while (renderer::stretched_tex_size_.y < window_size_y) {
+        increased_size = true;
+        renderer::stretched_tex_size_.y *= 2;
+    }
+
+    if (increased_size) {
+        UnloadRenderTexture(renderer::stretched_tex_);
+        renderer::stretched_tex_ =
+            LoadRenderTexture((int)renderer::stretched_tex_size_.x,
+                              (int)renderer::stretched_tex_size_.y);
+    }
 }
 
 // Base texture is the base low res pixel art game texture, with no
@@ -53,8 +83,10 @@ void renderer::render_base_() {
 
 // Stretched texture is the base texture stretched to fill the screen space.
 void renderer::render_stretched_() {
-    auto window_size_x = (float)GetRenderWidth();
-    auto window_size_y = (float)GetRenderHeight();
+    renderer::update_stretched_tex_size_();
+
+    float window_size_x = (float)GetRenderWidth();
+    float window_size_y = (float)GetRenderHeight();
     float aspect_ratio = (float)WINDOW_SIZE_X / (float)WINDOW_SIZE_Y;
 
     BeginTextureMode(stretched_tex_);
