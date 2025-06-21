4acbde61a4a26a0e70d532e61813e6e70ac4d38a
eldskald <rafaelbordoni00@gmail.com>
2025-06-21 01:55:32 -0300

    Rework data loader

--- src/core/data-loader.cpp
+++ src/core/data-loader.cpp
@@ -16,7 +16,6 @@
 #include "engine/text.h"
 #include <cctype>
 #include <exception>
-#include <iostream>
 #include <list>
 #include <raylib.h>
 #include <stdexcept>
@@ -26,7 +25,7 @@
 
 unordered_map<string, game_element*> data_loader::id_map_ = {};
 
-string data_loader::remove_whitespace(string text) {
+string data_loader::remove_whitespace_(string text) {
     for (int i = 0; i < (int)text.size(); ++i) {
         if (isspace(text[i])) {
             text.erase(i, 1);
@@ -36,38 +35,38 @@ string data_loader::remove_whitespace(string text) {
     return text;
 }
 
-vector<string> data_loader::split_into_lines_with_no_whitespace(string text) {
+vector<string> data_loader::split_into_lines_with_no_whitespace_(string text) {
     vector<string> lines;
     while (!text.empty()) {
         string line = text.substr(0, text.find('\n'));
         if (!line.empty())
-            lines.push_back(data_loader::remove_whitespace(line));
+            lines.push_back(data_loader::remove_whitespace_(line));
         text.erase(0, text.find('\n') + 1);
     }
     return lines;
 }
 
-bool data_loader::check_if_line_starts_a_block(string line) {
+bool data_loader::check_if_line_starts_a_block_(string line) {
     return line[0] == '[' && line[line.size() - 1] == ']';
 }
 
 data_loader::element_block
-data_loader::get_top_element_block(vector<string>* data) {
-    if (!data_loader::check_if_line_starts_a_block((*data)[0]))
+data_loader::get_top_element_block_(vector<string>* data) {
+    if (!data_loader::check_if_line_starts_a_block_((*data)[0]))
         throw invalid_argument("first line not a block starter");
 
     element_block block;
     block.type = (*data)[0].substr(1, (*data)[0].size() - 2);
     (*data).erase((*data).begin());
     while (!(*data).empty()) {
-        if (data_loader::check_if_line_starts_a_block((*data)[0])) break;
+        if (data_loader::check_if_line_starts_a_block_((*data)[0])) break;
         block.properties.push_back((*data)[0]);
         (*data).erase((*data).begin());
     }
     return block;
 }
 
-vector<string> data_loader::string_to_array(string str) {
+vector<string> data_loader::string_to_array_(string str) {
     vector<string> arr;
     while (true) {
         size_t next_semicolon = str.find(';');
@@ -79,7 +78,7 @@ vector<string> data_loader::string_to_array(string str) {
     return arr;
 }
 
-Vector2 data_loader::string_to_vector(string str) {
+Vector2 data_loader::string_to_vector_(string str) {
     if (str[0] != '(' || str[str.size() - 1] != ')')
         throw invalid_argument("value not a vector");
     if (str.find(',') == str.npos) throw invalid_argument("value not a vector");
@@ -98,7 +97,7 @@ Vector2 data_loader::string_to_vector(string str) {
     return vec;
 }
 
-Rectangle data_loader::string_to_rectangle(string str) {
+Rectangle data_loader::string_to_rectangle_(string str) {
     if (str[0] != '(' || str[str.size() - 1] != ')')
         throw invalid_argument("value not a rectangle");
 
@@ -138,7 +137,7 @@ Rectangle data_loader::string_to_rectangle(string str) {
     return rect;
 }
 
-Color data_loader::string_to_color(string str) {
+Color data_loader::string_to_color_(string str) {
     if (str[0] != '(' || str[str.size() - 1] != ')')
         throw invalid_argument("value not a color");
 
@@ -178,20 +177,20 @@ Color data_loader::string_to_color(string str) {
     return col;
 }
 
-bool data_loader::string_to_bool(string str) {
+bool data_loader::string_to_bool_(string str) {
     if (str == "true") return true;
     if (str == "false") return false;
     throw invalid_argument("value not a bool");
 }
 
-physics_body::body_type data_loader::string_to_body_type(string str) {
+physics_body::body_type data_loader::string_to_body_type_(string str) {
     if (str == "kinematic") return physics_body::kinematic;
     if (str == "fixed") return physics_body::fixed;
     if (str == "area") return physics_body::area;
     throw invalid_argument("value not a body type");
 }
 
-keyframe data_loader::string_to_keyframe(string str) {
+keyframe data_loader::string_to_keyframe_(string str) {
     if (str[0] != '(' || str[str.size() - 1] != ')')
         throw invalid_argument("value not a keyframe");
     string data = str.substr(1, str.size() - 2);
@@ -203,21 +202,21 @@ keyframe data_loader::string_to_keyframe(string str) {
     string duration_element = data.substr(1, data.size() - 1);
 
     keyframe frame = (keyframe){(Vector2){0}, 0.0f};
-    frame.atlas_coords = data_loader::string_to_vector(atlas_coords_element);
+    frame.atlas_coords = data_loader::string_to_vector_(atlas_coords_element);
     frame.duration = stof(duration_element);
     return frame;
 }
 
-animation data_loader::string_to_animation(string str) {
-    vector<string> frames_string = data_loader::string_to_array(str);
+animation data_loader::string_to_animation_(string str) {
+    vector<string> frames_string = data_loader::string_to_array_(str);
     animation anim;
     for (string frame_string : frames_string) {
-        anim.push_back(data_loader::string_to_keyframe(frame_string));
+        anim.push_back(data_loader::string_to_keyframe_(frame_string));
     }
     return anim;
 }
 
-string data_loader::string_to_string(string str) {
+string data_loader::string_to_string_(string str) {
     string r = "";
     for (char i : str) {
         r += i == '@' ? ' ' : i;
@@ -225,14 +224,14 @@ string data_loader::string_to_string(string str) {
     return r;
 }
 
-void data_loader::add_parent(game_element* element, string id) {
+void data_loader::add_parent_(game_element* element, string id) {
     if (data_loader::id_map_.count(id) == 0)
         throw invalid_argument("invalid parent id");
     data_loader::id_map_[id]->add_child(element);
 }
 
-void data_loader::parse_game_element_property_line(game_element* element,
-                                                   string line) {
+void data_loader::parse_game_element_property_line_(game_element* element,
+                                                    string line) {
     if (line.find('=') == line.npos)
         throw invalid_argument("property line has no '=' sign");
 
@@ -242,15 +241,15 @@ void data_loader::parse_game_element_property_line(game_element* element,
     if (prop_name == "id") {
         data_loader::id_map_[prop_value] = element;
     } else if (prop_name == "parent") {
-        data_loader::add_parent(element, prop_value);
+        data_loader::add_parent_(element, prop_value);
     } else if (prop_name == "pos") {
-        element->pos = data_loader::string_to_vector(prop_value);
+        element->pos = data_loader::string_to_vector_(prop_value);
     } else
         throw invalid_argument("invalid property name");
 }
 
-void data_loader::parse_physics_body_property_line(physics_body* body,
-                                                   string line) {
+void data_loader::parse_physics_body_property_line_(physics_body* body,
+                                                    string line) {
     if (line.find('=') == line.npos)
         throw invalid_argument("property line has no '=' sign");
 
@@ -260,15 +259,15 @@ void data_loader::parse_physics_body_property_line(physics_body* body,
     if (prop_name == "id") {
         data_loader::id_map_[prop_value] = body;
     } else if (prop_name == "parent") {
-        data_loader::add_parent(body, prop_value);
+        data_loader::add_parent_(body, prop_value);
     } else if (prop_name == "pos") {
-        body->pos = data_loader::string_to_vector(prop_value);
+        body->pos = data_loader::string_to_vector_(prop_value);
     } else if (prop_name == "type") {
-        body->type = data_loader::string_to_body_type(prop_value);
+        body->type = data_loader::string_to_body_type_(prop_value);
     } else if (prop_name == "one_way") {
-        body->one_way = data_loader::string_to_bool(prop_value);
+        body->one_way = data_loader::string_to_bool_(prop_value);
     } else if (prop_name == "collision_box") {
-        body->collision_box = data_loader::string_to_rectangle(prop_value);
+        body->collision_box = data_loader::string_to_rectangle_(prop_value);
     } else if (prop_name == "collision_layer") {
         body->collision_layer = bitset<COLLISION_LAYERS>(prop_value);
     } else if (prop_name == "collision_mask") {
@@ -277,7 +276,7 @@ void data_loader::parse_physics_body_property_line(physics_body* body,
         throw invalid_argument("invalid property name");
 }
 
-void data_loader::parse_sprite_property_line(sprite* sprite, string line) {
+void data_loader::parse_sprite_property_line_(sprite* sprite, string line) {
     if (line.find('=') == line.npos)
         throw invalid_argument("property line has no '=' sign");
 
@@ -287,24 +286,24 @@ void data_loader::parse_sprite_property_line(sprite* sprite, string line) {
     if (prop_name == "id") {
         data_loader::id_map_[prop_value] = sprite;
     } else if (prop_name == "parent") {
-        data_loader::add_parent(sprite, prop_value);
+        data_loader::add_parent_(sprite, prop_value);
     } else if (prop_name == "pos") {
-        sprite->pos = data_loader::string_to_vector(prop_value);
+        sprite->pos = data_loader::string_to_vector_(prop_value);
     } else if (prop_name == "atlas_coords") {
-        sprite->atlas_coords = data_loader::string_to_vector(prop_value);
+        sprite->atlas_coords = data_loader::string_to_vector_(prop_value);
     } else if (prop_name == "tint") {
-        sprite->tint = data_loader::string_to_color(prop_value);
+        sprite->tint = data_loader::string_to_color_(prop_value);
     } else if (prop_name == "z_index") {
         sprite->z_index = stoi(prop_value);
     } else if (prop_name == "animation") {
-        sprite->anim = data_loader::string_to_animation(prop_value);
+        sprite->anim = data_loader::string_to_animation_(prop_value);
     } else if (prop_name == "animation_starting_phase") {
         sprite->animation_starting_phase = stof(prop_value);
     } else
         throw invalid_argument("invalid property name");
 }
 
-void data_loader::parse_text_property_line(text* text, string line) {
+void data_loader::parse_text_property_line_(text* text, string line) {
     if (line.find('=') == line.npos)
         throw invalid_argument("property line has no '=' sign");
 
@@ -314,17 +313,17 @@ void data_loader::parse_text_property_line(text* text, string line) {
     if (prop_name == "id") {
         data_loader::id_map_[prop_value] = text;
     } else if (prop_name == "parent") {
-        data_loader::add_parent(text, prop_value);
+        data_loader::add_parent_(text, prop_value);
     } else if (prop_name == "pos") {
-        text->pos = data_loader::string_to_vector(prop_value);
+        text->pos = data_loader::string_to_vector_(prop_value);
     } else if (prop_name == "content") {
-        text->content = data_loader::string_to_string(prop_value);
+        text->content = data_loader::string_to_string_(prop_value);
     } else if (prop_name == "hidden_for") {
         text->hidden_for = stof(prop_value);
     }
 }
 
-void data_loader::parse_player_property_line(player* player, string line) {
+void data_loader::parse_player_property_line_(player* player, string line) {
     if (line.find('=') == line.npos)
         throw invalid_argument("property line has no '=' sign");
 
@@ -334,13 +333,13 @@ void data_loader::parse_player_property_line(player* player, string line) {
     if (prop_name == "id") {
         data_loader::id_map_[prop_value] = player;
     } else if (prop_name == "parent") {
-        data_loader::add_parent(player, prop_value);
+        data_loader::add_parent_(player, prop_value);
     } else if (prop_name == "pos") {
-        player->pos = data_loader::string_to_vector(prop_value);
+        player->pos = data_loader::string_to_vector_(prop_value);
     }
 }
 
-void data_loader::parse_water_property_line(water* water, string line) {
+void data_loader::parse_water_property_line_(water* water, string line) {
     if (line.find('=') == line.npos)
         throw invalid_argument("property line has no '=' sign");
 
@@ -352,7 +351,7 @@ void data_loader::parse_water_property_line(water* water, string line) {
     }
 }
 
-void data_loader::parse_key_gate_property_line(key_gate* gate, string line) {
+void data_loader::parse_key_gate_property_line_(key_gate* gate, string line) {
     if (line.find('=') == line.npos)
         throw invalid_argument("property line has no '=' sign");
 
@@ -360,109 +359,109 @@ void data_loader::parse_key_gate_property_line(key_gate* gate, string line) {
     string prop_value =
         line.substr(line.find('=') + 1, line.size() - line.find('=') - 1);
     if (prop_name == "pos") {
-        gate->pos = data_loader::string_to_vector(prop_value);
+        gate->pos = data_loader::string_to_vector_(prop_value);
     } else if (prop_name == "height") {
         gate->height = stof(prop_value);
     } else if (prop_name == "key_positions") {
-        for (string str_value : data_loader::string_to_array(prop_value)) {
+        for (string str_value : data_loader::string_to_array_(prop_value)) {
             gate->key_positions.push_back(
-                data_loader::string_to_vector(str_value));
+                data_loader::string_to_vector_(str_value));
         }
     }
 }
 
-void data_loader::parse_level_shadows_limit_line(string line) {
+void data_loader::parse_level_shadows_limit_line_(string line) {
     scene_manager::set_shadows_limit(stoi(line));
 }
 
-game_element* data_loader::get_element_from_block(element_block block) {
+game_element* data_loader::get_element_from_block_(element_block block) {
     if (block.type == "game_element") {
         auto* obj = new game_element();
         for (auto& line : block.properties) {
-            data_loader::parse_game_element_property_line(obj, line);
+            data_loader::parse_game_element_property_line_(obj, line);
         }
         return obj;
     }
     if (block.type == "physics_body") {
         auto* body = new physics_body();
         for (auto& line : block.properties) {
-            data_loader::parse_physics_body_property_line(body, line);
+            data_loader::parse_physics_body_property_line_(body, line);
         }
         return body;
     }
     if (block.type == "sprite") {
         auto* img = new sprite();
         for (auto& line : block.properties) {
-            data_loader::parse_sprite_property_line(img, line);
+            data_loader::parse_sprite_property_line_(img, line);
         }
         return img;
     }
     if (block.type == "text") {
         auto* txt = new text();
         for (auto& line : block.properties) {
-            data_loader::parse_text_property_line(txt, line);
+            data_loader::parse_text_property_line_(txt, line);
         }
         return txt;
     }
     if (block.type == "player") {
         auto* player_element = new player();
         for (auto& line : block.properties) {
-            data_loader::parse_player_property_line(player_element, line);
+            data_loader::parse_player_property_line_(player_element, line);
         }
         return player_element;
     }
     if (block.type == "goal") {
         auto* goal_element = new goal();
         for (auto& line : block.properties) {
-            data_loader::parse_physics_body_property_line(goal_element, line);
+            data_loader::parse_physics_body_property_line_(goal_element, line);
         }
         return goal_element;
     }
     if (block.type == "water") {
         auto* water_element = new water();
         for (auto& line : block.properties) {
-            data_loader::parse_water_property_line(water_element, line);
+            data_loader::parse_water_property_line_(water_element, line);
         }
         return water_element;
     }
     if (block.type == "falling_leaves") {
         auto* emitter = new falling_leaves();
         for (auto& line : block.properties) {
-            data_loader::parse_game_element_property_line(emitter, line);
+            data_loader::parse_game_element_property_line_(emitter, line);
         }
         return emitter;
     }
     if (block.type == "water_drip") {
         auto* emitter = new water_drip();
         for (auto& line : block.properties) {
-            data_loader::parse_game_element_property_line(emitter, line);
+            data_loader::parse_game_element_property_line_(emitter, line);
         }
         return emitter;
     }
     if (block.type == "puff") {
         auto* emitter = new puff();
         for (auto& line : block.properties) {
-            data_loader::parse_game_element_property_line(emitter, line);
+            data_loader::parse_game_element_property_line_(emitter, line);
         }
         return emitter;
     }
     if (block.type == "killbox") {
         auto* killer = new killbox();
         for (auto& line : block.properties) {
-            data_loader::parse_physics_body_property_line(killer, line);
+            data_loader::parse_physics_body_property_line_(killer, line);
         }
         return killer;
     }
     if (block.type == "key_gate") {
         auto* gate = new key_gate();
         for (auto& line : block.properties) {
-            data_loader::parse_key_gate_property_line(gate, line);
+            data_loader::parse_key_gate_property_line_(gate, line);
         }
         return gate;
     }
     if (block.type == "shadows_limit") {
         for (auto& line : block.properties) {
-            data_loader::parse_level_shadows_limit_line(line);
+            data_loader::parse_level_shadows_limit_line_(line);
         }
         return nullptr;
     }
@@ -479,22 +478,23 @@ list<game_element*> data_loader::load(const char* file) {
     char* text = LoadFileText(file);
 
     vector<string> lines =
-        data_loader::split_into_lines_with_no_whitespace(text);
+        data_loader::split_into_lines_with_no_whitespace_(text);
     list<element_block> blocks;
     list<game_element*> elements;
     try {
         while (!lines.empty()) {
-            element_block block = data_loader::get_top_element_block(&lines);
+            element_block block = data_loader::get_top_element_block_(&lines);
             blocks.push_back(block);
         }
         for (auto block : blocks) {
-            game_element* element = data_loader::get_element_from_block(block);
+            game_element* element = data_loader::get_element_from_block_(block);
             if (element) elements.push_back(element);
         }
     } catch (invalid_argument& e) {
-        cout << e.what() << endl;
+        TraceLog(LOG_FATAL, e.what());
         CloseWindow();
     }
+    TraceLog(LOG_INFO, "Scene loaded successfully");
 
     UnloadFileText(text);
     return elements;
--- src/core/data-loader.h
+++ src/core/data-loader.h
@@ -23,33 +23,33 @@ private:
 
     static unordered_map<string, game_element*> id_map_;
 
-    static string remove_whitespace(string text);
-    static vector<string> split_into_lines_with_no_whitespace(string text);
+    static string remove_whitespace_(string text);
+    static vector<string> split_into_lines_with_no_whitespace_(string text);
 
-    static bool check_if_line_starts_a_block(string line);
-    static element_block get_top_element_block(vector<string>* data);
-    static game_element* get_element_from_block(element_block block);
+    static bool check_if_line_starts_a_block_(string line);
+    static element_block get_top_element_block_(vector<string>* data);
+    static game_element* get_element_from_block_(element_block block);
 
-    static vector<string> string_to_array(string str);
-    static Vector2 string_to_vector(string str);
-    static Rectangle string_to_rectangle(string str);
-    static Color string_to_color(string str);
-    static bool string_to_bool(string str);
-    static physics_body::body_type string_to_body_type(string str);
-    static keyframe string_to_keyframe(string str);
-    static animation string_to_animation(string str);
-    static string string_to_string(string str);
+    static vector<string> string_to_array_(string str);
+    static Vector2 string_to_vector_(string str);
+    static Rectangle string_to_rectangle_(string str);
+    static Color string_to_color_(string str);
+    static bool string_to_bool_(string str);
+    static physics_body::body_type string_to_body_type_(string str);
+    static keyframe string_to_keyframe_(string str);
+    static animation string_to_animation_(string str);
+    static string string_to_string_(string str);
 
-    static void add_parent(game_element* element, string id);
+    static void add_parent_(game_element* element, string id);
 
-    static void parse_game_element_property_line(game_element* element,
-                                                 string line);
-    static void parse_physics_body_property_line(physics_body* body,
-                                                 string line);
-    static void parse_sprite_property_line(sprite* sprite, string line);
-    static void parse_text_property_line(text* text, string line);
-    static void parse_player_property_line(player* player, string line);
-    static void parse_water_property_line(water* water, string line);
-    static void parse_key_gate_property_line(key_gate* gate, string line);
-    static void parse_level_shadows_limit_line(string line);
+    static void parse_game_element_property_line_(game_element* element,
+                                                  string line);
+    static void parse_physics_body_property_line_(physics_body* body,
+                                                  string line);
+    static void parse_sprite_property_line_(sprite* sprite, string line);
+    static void parse_text_property_line_(text* text, string line);
+    static void parse_player_property_line_(player* player, string line);
+    static void parse_water_property_line_(water* water, string line);
+    static void parse_key_gate_property_line_(key_gate* gate, string line);
+    static void parse_level_shadows_limit_line_(string line);
 };
